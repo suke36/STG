bf45175543793e58cfd6cc4624bb8b80512123d7
STG <mid.y.island@gmail.com>
2019-07-07 22:02:02 +0900

    これまでのやつ

---
+++ Assets/Scripts/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CommonParts
+{
+    public class BaseClass : MonoBehaviour
+    {
+		#region フィールド
+		private double dblHP;
+		#endregion
+
+		#region プロパティ
+		public virtual double HP { get => dblHP; set => dblHP = value; }
+		public virtual Vector3 Position { get => transform.position; set => transform.position = value; }
+		public virtual Quaternion Rotation { get => transform.rotation; set => transform.rotation = value; }
+		public Transform Transform { get => transform; }
+		#endregion
+
+	}
+}
