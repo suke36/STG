ea50b65a095d7574984c5772dfeb36f52e7d6f40
STG <mid.y.island@gmail.com>
2019-07-15 20:18:22 +0900

    共通クラスをpartialで分けた　だけ

--- Assets/Scripts/BaseClass.cs
+++ Assets/Scripts/BaseClass.cs
@@ -2,14 +2,9 @@
 
 namespace CommonParts
 {
-    public class BaseClass : MonoBehaviour
+    public partial class BaseClass : MonoBehaviour
     {
-		#region フィールド
-		private double dblHP;
-		#endregion
-
 		#region プロパティ
-		public virtual double HP { get => dblHP; set => dblHP = value; }
 		public virtual Vector3 Position { get => transform.position; set => transform.position = value; }
 		public virtual Quaternion Rotation { get => transform.rotation; set => transform.rotation = value; }
 		public Transform Transform { get => transform; }
---
+++ Assets/Scripts/BaseClass_CommonParts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CommonParts
+{
+	public partial class BaseClass
+	{
+		#region フィールド
+		private double hp;
+		#endregion
+
+		#region プロパティ
+		public double HP { get => hp; set => hp = value; }
+		#endregion
+	}
+}
