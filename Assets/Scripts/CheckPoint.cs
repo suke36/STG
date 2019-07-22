12836f8b98cd9c825a05b299d2fd34e6dd9c3f3f
STG <mid.y.island@gmail.com>
2019-07-22 20:47:22 +0900

    チェックポイント１

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using CommonParts;
+
+namespace Check
+{
+	enum MoveMode
+	{
+		Straight = 0,
+		Curve,
+	}
+	
+	public class CheckPoint : BaseClass
+	{
+		CheckPoint next;
+
+		MoveMode Mode;
+
+		public CheckPoint Next { get => next; set => next = value; }
+	}
+}
---
+++ Assets/Scripts/ScrollManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CommonParts;
+using Check;
+
+public class ScrollManager : BaseClass
+{
+	/// <summary>
+	/// このnextを追っていく
+	/// 追いついたらnextのnext(lol)
+	/// </summary>
+	CheckPoint next;
+}
