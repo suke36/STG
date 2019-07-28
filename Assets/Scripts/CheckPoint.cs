d2b7ed0762054802f15d0853eb34ea46f9f3fa21
STG <mid.y.island@gmail.com>
2019-07-28 22:14:35 +0900

    7/28

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,19 +1,18 @@
 ﻿using CommonParts;
+using UnityEngine;
 
-namespace Check
+
+enum MoveMode
 {
-	enum MoveMode
-	{
-		Straight = 0,
-		Curve,
-	}
-	
-	public class CheckPoint : BaseClass
-	{
-		CheckPoint next;
-
-		MoveMode Mode;
-
-		public CheckPoint Next { get => next; set => next = value; }
-	}
+	Straight = 0,
+	Curve,
 }
+
+public class CheckPoint : BaseClass
+{
+	public GameObject[] Nexts;
+
+	MoveMode Mode;
+
+}
+
--- Assets/Scripts/ScrollManager.cs
+++ Assets/Scripts/ScrollManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CommonParts;
-using Check;
 
 public class ScrollManager : BaseClass
 {
@@ -10,5 +9,33 @@ public class ScrollManager : BaseClass
 	/// このnextを追っていく
 	/// 追いついたらnextのnext(lol)
 	/// </summary>
-	CheckPoint next;
+	GameObject next;
+
+	/// <summary>
+	/// 次のチェックポイントまでの到達時間
+	/// </summary>
+	public float ScrollSpeed;
+
+
+
+	void Update()
+	{
+		// 追従対象オブジェクトのTransformから、目的地を算出
+		Vector3 targetPos = next.GetComponent<CheckPoint>().Position;
+		Quaternion targetRot = next.GetComponent<CheckPoint>().Rotation;
+
+		//ref
+		Vector3 velocity = Vector3.zero;
+		float fvelocity = 0;
+		// 移動
+		Position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, ScrollSpeed);
+
+		//回転
+		Quaternion rot = new Quaternion();
+		rot.x = Mathf.SmoothDampAngle(Rotation.x, targetRot.x, ref fvelocity, ScrollSpeed);
+		rot.y = Mathf.SmoothDampAngle(Rotation.y, targetRot.y, ref fvelocity, ScrollSpeed);
+		rot.z = Mathf.SmoothDampAngle(Rotation.z, targetRot.z, ref fvelocity, ScrollSpeed);
+		Rotation = rot;
+
+	}
 }
